4b5b07c3e2b3379c812742d27bf75d275f775d98
MH Dip <mhdip.pro@gmail.com>
2020-09-06 00:49:07 +0600

    template rendering with data

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import *
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    pending = orders.filter(status='Pending').count()
+    delivered = orders.filter(status='Delivered').count()
+    context = {
+        'orders': orders, 
+        'customers':customers,
+        'total_orders':total_orders,
+        'total_customers':total_customers,
+        'delivered':delivered,
+        'pending':pending
+        }
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    context = {'products' : products}
+    return render(request, 'accounts/products.html', context)
 
 def customer(request):
     return render(request, 'accounts/customer.html')
